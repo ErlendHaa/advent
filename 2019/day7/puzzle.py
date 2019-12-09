8d908f22a9f090134f3e3fe9b5309a935a8ef123
Erlend Hårstad <erha@equinor.com>
2019-12-09 14:26:13 +0100

    Day 7: Amplification Circuit, part 2
    After some pointers on how to decipher the puzzle description from a
    collegue I finally managed to complete part 2.

--- 2019/day7/puzzle.py
+++ 2019/day7/puzzle.py
@@ -81,202 +81,133 @@ def parse(path):
     with open(path, mode='r') as f:
         return [int(x) for x in f.readline().split(',')]
 
-class amp:
-    def __init__(self, name, program, phase):
+class computer:
+    def __init__(self, program, inputs):
         self.program = list(program)
-        self.name    = name
-        self.inputs  = deque()
-        self.phase   = phase
-        self.output  = []
         self.ip      = 0
+        self.outputs = []
+        self.inputs  = deque(inputs)
+        self.paused  = False
 
-    def __repr__(self):
-        return self.name
-
-    def putin(self, value):
-        self.inputs = deque([value])
-
-    def popin(self):
-        return self.inputs.popleft()
-
-    def popout(self):
-        out = self.output
-        self.output = []
-        return out
+        ext = [0] * 1000000
+        self.program.extend(ext)
 
     def run(self):
-        opcode = 0
-        while opcode != 99:
-            opcode, C, B, A = instruction( self.program, self.ip )
-            if   opcode == 1:
-                self.ip = add(self.program, self.ip, C, B, A)
-            elif opcode == 2:
-                self.ip = mul(self.program, self.ip, C, B, A)
-            elif opcode == 3:
-                inp = self.inputs.popleft()
-                self.ip = put(self.program, self.ip, inp )
-            elif opcode == 4:
-                self.ip, out = get(self.program, self.ip)
-                yield out
-            elif opcode == 5:
-                self.ip = jumpt(program, self.ip, C, B)
-            elif opcode == 6:
-                self.ip = jumpf(program, self.ip, C, B)
-            elif opcode == 7:
-                self.ip = less( self.program, self.ip, C, B, A)
-            elif opcode == 8:
-                self.ip = equal(program, self.ip, C, B, A)
+        self.paused = False
+        opcode = None
+
+        while opcode != 99 and not self.paused:
+            opcode, c, b, a = self.instruction()
+            if   opcode == 1: self.add(c, b, a)
+            elif opcode == 2: self.mul(c, b, a)
+            elif opcode == 3: self.put()
+            elif opcode == 4: self.get(c)
+            elif opcode == 5: self.jumpt(c, b)
+            elif opcode == 6: self.jumpf(c, b)
+            elif opcode == 7: self.less(c, b, a)
+            elif opcode == 8: self.equal(c, b, a)
             else:
-                msg = 'Something went wrong in amp {}, found value {} in address {}, current output {}'
-                logging.warning(msg.format(self.name, opcode, self.ip,
-                    self.output))
-                raise
-        raise StopIteration
+                break
+        return self
 
-def feedback(program, inputs, init=0):
-    a, b, c, d, e = tuple(inputs)
-    A = amp('a', program, phase=a)
-    B = amp('b', program, phase=b)
-    C = amp('c', program, phase=c)
-    D = amp('d', program, phase=d)
-    E = amp('e', program, phase=e)
+    def instruction(self):
+        opcode = 10*((self.program[self.ip] // 10) % 10) + self.program[self.ip] % 10
+        c = (self.program[self.ip] // 100)  % 10
+        b = (self.program[self.ip] // 1000) % 10
+        a = (self.program[self.ip] // 10000)
 
-    output = []
-    amps = deque([A, B, C, D, E])
-    value = init
-    while amps:
-        cur = amps.popleft()
-        cur.putin(value)
-        try:
-            value = next(cur.run())
-            amps.append(cur)
-        except StopIteration:
-            break
+        if opcode == 99: return opcode, c, b, a
 
-    return value
+        if   c == 0: c = self.program[self.program[self.ip + 1]]
+        elif c == 1: c = self.program[self.ip + 1]
 
-def run(program, inputs):
-    ip, pin = 0, 0
-    output = []
+        if   b == 0: b = self.program[self.program[self.ip + 2] ]
+        elif b == 1: b = self.program[self.ip + 2]
 
-    while True:
-        opcode, C, B, A = instruction( program, ip )
-        if   opcode == 1: ip = add(  program, ip, C, B, A)
-        elif opcode == 2: ip = mul(  program, ip, C, B, A)
-        elif opcode == 4: ip, output = get(  program, ip)
-        elif opcode == 5: ip = jumpt(program, ip, C, B)
-        elif opcode == 6: ip = jumpf(program, ip, C, B)
-        elif opcode == 7: ip = less( program, ip, C, B, A)
-        elif opcode == 8: ip = equal(program, ip, C, B, A)
-        elif opcode == 3:
-            ip = put(  program, ip, inputs[pin])
-            pin += 1
-        elif opcode == 99:
-            msg = 'Program finished with code 99, output: {}'
-            logging.warning(msg.format(output[0]))
-            break
-        else:
-            break
+        if   a == 0: a = self.program[self.ip + 3]
+        elif a == 1: a = self.program[self.ip + 3]
 
-    return output
+        return opcode, c, b, a
 
-def instruction(program, ip):
-    opcode, A, B, C = 0, 0, 0, 0
-    inst = [int(x) for x in str(program[ip])]
-    opcode = inst[-1]
+    def add(self, c, b, a):
+        self.program[a] = c + b
+        self.ip += 4
 
-    try: C = inst[-3]
-    except: pass
+    def mul(self, c, b, a):
+        self.program[a] = c * b
+        self.ip += 4
 
-    try: B = inst[-4]
-    except: pass
+    def put(self):
+        if len(self.inputs) == 0:
+            self.paused = True
+            return
 
-    try: A = inst[-5]
-    except: pass
+        addr = self.program[self.ip + 1]
+        self.program[ addr ] = self.inputs.popleft()
+        self.ip += 2
 
-    try:
-        if C == 0: C = program[ program[ip + 1] ]
-        else:      C = program[ ip + 1]
-    except: pass
+    def get(self, c):
+        self.outputs.append(c)
+        self.ip += 2
 
-    try:
-        if B == 0: B = program[ program[ip + 2] ]
-        else:      B = program[ ip + 2]
-    except: pass
+    def jumpt(self, c, b):
+        if c != 0: self.ip  = b
+        else:      self.ip += 3
 
-    try:
-        if A == 0: A = program[ip + 3]
-        else:      A = ip + 3
-    except: pass
+    def jumpf(self, c, b):
+        if c == 0: self.ip  = b
+        else:      self.ip += 3
 
-    return opcode, C, B, A
+    def less(self, c, b, a):
+        if c < b: self.program[a] = 1
+        else:     self.program[a] = 0
+        self.ip += 4
 
-
-def add(program, ip, c, b, a):
-    program[a] = c + b
-    return ip + 4
-
-def mul(program, ip, c, b, a):
-    program[a] = c * b
-    return ip + 4
-
-def put(program, ip, val):
-    program[ program[ ip + 1] ] = val
-    return ip + 2
-
-def get(program, ip):
-    val = program[ program[ ip + 1] ]
-    return ip + 2, val
-
-def jumpt(program, ip, c, b):
-    if c != 0: return  b
-    else:      return ip + 3
-
-def jumpf(program, ip, c, b):
-    if c == 0: return b
-    else:      return ip + 3
-
-def less(program, ip, c, b, a):
-    if c < b: program[a] = 1
-    else:     program[a] = 0
-    return ip + 4
-
-def equal(program, ip, c, b, a):
-    if c == b: program[a] = 1
-    else:      program[a] = 0
-    return ip + 4
-
-def amplifiers(program, inputs, testid=0):
-    a, b, c, d, e = tuple(inputs)
-    return output
+    def equal(self, c, b, a):
+        if c == b: self.program[a] = 1
+        else:      self.program[a] = 0
+        self.ip += 4
 
 def part1(program, testid=0):
     out = float('-inf')
     for a,b,c,d,e in itertools.permutations(range(5)):
-        output = run(program,[a, testid])
-        output = run(program,[b, output])
-        output = run(program,[c, output])
-        output = run(program,[d, output])
-        output = run(program,[e, output])
+        amp_a = computer(program,[a, testid])
+        amp_a.run()
+        amp_b = computer(program,[b, amp_a.outputs[0]])
+        amp_b.run()
+        amp_c = computer(program,[c, amp_b.outputs[0]])
+        amp_c.run()
+        amp_d = computer(program,[d, amp_c.outputs[0]])
+        amp_d.run()
+        amp_e = computer(program,[e, amp_d.outputs[0]])
+        amp_e.run()
+        output = amp_e.outputs[0]
         if output > out: out = output
     return out
 
-def part2(program):
+def part2(program, init=0):
     outputs = []
     for inputs in itertools.permutations(range(5, 10)):
-        out = feedback(program, inputs)
-        outputs.append(out)
-    return outputs
+        amps = [computer(program, [x]).run() for x in inputs]
+        out  = init
+        halted = False
+
+        while not halted:
+            for amp in amps:
+                amp.inputs.append(out)
+                amp.run()
+                out = amp.outputs[-1]
+
+                if amp.paused: continue
+
+                halted = True
+
+            outputs.append(amps[-1].outputs[-1])
+
+    return max(outputs)
 
 if __name__ == '__main__':
     program = parse('input.txt')
+
     print('Part 1: {}'.format(part1(program)))
-
-    program = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
-            27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
-    program =[
-            3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
-            -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
-            53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
-
-    #print('Part 2: {}'.format(part2(program)))
+    print('Part 2: {}'.format(part2(program)))
